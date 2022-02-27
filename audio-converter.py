0d8e387f0042ea88eeda31eb8b5b081f9d785da0
tooru-furuyama <52897124+tooru-furuyama@users.noreply.github.com>
2022-02-27 12:50:58 +0000

    Updated (2022-02-27)
    - Improved the code -- applied "private" attribute to some variables and functions
    - Added command line parameters
    - Fixed some cosmetic issues

--- audio-converter.py
+++ audio-converter.py
@@ -7,59 +7,72 @@ from pathlib import Path
 class Resampling:
 
     def __init__(self):
-        self.debug = False
-        self.ffmpeg_command = 'ffmpeg'
-        self.ffmpeg_option = ''
-        self.src_filetype = 'flac'
-        self.out_filetype = 'flac'
-        self.profile_json = 'config.json'
+        self.__debug = False
+        self.__ffmpeg_command = 'ffmpeg'
+        self.__ffmpeg_options = ''
+        self.__src_filetype = ''
+        self.__out_filetype = ''
+        self.__profile_json = 'config.json'
 
 
     def set_profile(self, profile):
-        self.profile_json = profile
+        self.__profile_json = profile
+        return
+
+    def set_input_filetype(self, in_filetype):
+        self.__src_filetype = in_filetype.lower()
+        self.__debug_print__('Input filetype  :' + self.__src_filetype)
+        return
+
+    def set_output_filetype(self, out_filetype):
+        self.__out_filetype = out_filetype.lower()
+        self.__debug_print__('Output filetype :' + self.__out_filetype)
         return
 
 
     def configure(self):
-        profile_path = os.path.split(__file__)[0] + os.sep + self.profile_json
-        self.debug_print(profile_path)
+        profile_path = os.path.split(__file__)[0] + os.sep + self.__profile_json
+        self.__debug_print__(profile_path)
 
         try:
             config_file = open(profile_path, 'r')
             config_data = json.load(config_file)
-            self.debug_print(profile_path)
+            self.__debug_print__(profile_path)
 
-            self.debug = config_data['exec_options']['debug_option']
-            self.ffmpeg_command = config_data['exec_options']['ffmpeg_path']
-            self.debug_print(self.ffmpeg_command)
+            self.__debug = config_data['exec_options']['debug_option']
+            self.__ffmpeg_command = config_data['exec_options']['ffmpeg_path']
+            self.__debug_print__(self.__ffmpeg_command)
 
-            self.ffmpeg_command = config_data['exec_options']['ffmpeg_path']
-            self.src_filetype = config_data['input_options']['src_type'].lower()
-            self.out_filetype = config_data['output_options']['out_type'].lower()
-            self.debug_print('Input filetype  :' + self.src_filetype)
-            self.debug_print('Output filetype :' + self.out_filetype)
+            self.__ffmpeg_command = config_data['exec_options']['ffmpeg_path']
+            if not len(self.__src_filetype) > 0:
+                self.__src_filetype = config_data['input_options']['src_type'].lower()
+            if not len(self.__out_filetype) > 0:
+                self.__out_filetype = config_data['output_options']['out_type'].lower()
 
-            if self.out_filetype == 'flac':
+            self.__debug_print__('Input filetype  :' + self.__src_filetype)
+            self.__debug_print__('Output filetype :' + self.__out_filetype)
+
+            if self.__out_filetype == 'flac':
                 if config_data['output_options']['flac_options']['resampling']:
                     out_bit_depth = config_data['output_options']['flac_options']['resampling_options']['bit_depth']
                     out_sampling = config_data['output_options']['flac_options']['resampling_options']['sampling']
                     out_resampler = 'aresample=resampler=' + config_data['output_options']['flac_options']['resampling_options']['resampler']
                     ffmpeg_resample_option = ' '.join(['-sample_fmt', out_bit_depth, '-af', out_resampler, '-ar', out_sampling])
-                    self.debug_print(ffmpeg_resample_option)
-                    self.ffmpeg_option = ffmpeg_resample_option
+                    self.__debug_print__(ffmpeg_resample_option)
+                    self.__ffmpeg_options = ffmpeg_resample_option
 
                 comp_level = config_data['output_options']['flac_options']['comp_level']
                 ffmpeg_comp_option = ' '.join(['-compression_level', comp_level])
-                self.ffmpeg_option = ' '.join([self.ffmpeg_option, ffmpeg_comp_option, '-c:v copy'])
-                self.debug_print(self.ffmpeg_option)
+                self.__ffmpeg_options = ' '.join([self.__ffmpeg_options, ffmpeg_comp_option, '-c:v copy'])
+                self.__debug_print__(self.__ffmpeg_options)
 
-            elif self.out_filetype == 'mp3':
-                self.ffmpeg_option = ' '.join([self.ffmpeg_option, '-c:a', config_data['output_options']['mp3_options']['codec'], config_data['output_options']['mp3_options']['option_string'], '-c:v copy'])
-                self.debug_print(self.ffmpeg_option)
+            elif self.__out_filetype == 'mp3':
+                self.__ffmpeg_options = ' '.join([self.__ffmpeg_options, '-c:a', config_data['output_options']['mp3_options']['codec'], config_data['output_options']['mp3_options']['option_string'], '-c:v copy'])
+                self.__debug_print__(self.__ffmpeg_options)
 
-            elif self.out_filetype == 'aac':
-                self.ffmpeg_option = ' '.join([self.ffmpeg_option, '-c:a', config_data['output_options']['aac_options']['codec'], config_data['output_options']['aac_options']['option_string'], '-c:v copy'])
-                self.debug_print(self.ffmpeg_option)
+            elif self.__out_filetype == 'aac':
+                self.__ffmpeg_options = ' '.join([self.__ffmpeg_options, '-c:a', config_data['output_options']['aac_options']['codec'], config_data['output_options']['aac_options']['option_string'], '-c:v copy'])
+                self.__debug_print__(self.__ffmpeg_options)
 
         except:
             print('Error')
@@ -68,21 +81,23 @@ class Resampling:
 
 
     def exec(self):
-        target_file = '**/*.' + self.src_filetype
+        target_file = '*.' + self.__src_filetype
         filelist = Path(os.getcwd()).glob(target_file)
+        self.__debug_print__(filelist)
         out_dir = os.path.join(os.getcwd(), 'out')
-        self.debug_print(out_dir)
-        try:
-            os.mkdir(out_dir)
-        except:
-            print('Output directory already exist')
+        self.__debug_print__(out_dir)
+        if not os.path.exists(out_dir):
+            try:
+                os.mkdir(out_dir)
+            except:
+                print('Output directory creation failed')
 
         for file in filelist:
             try:
                 in_filename = '\"' + file.name + '\"'
-                out_filename = '\"' + 'out' + os.sep + file.name.rsplit('.', 1)[0] + '.' + self.out_filetype + '\"'
-                shell_command = ' '.join([self.ffmpeg_command, '-i', in_filename, self.ffmpeg_option, out_filename])
-                self.debug_print(shell_command)
+                out_filename = '\"' + 'out' + os.sep + file.name.rsplit('.', 1)[0] + '.' + self.__out_filetype + '\"'
+                shell_command = ' '.join([self.__ffmpeg_command, '-i', in_filename, self.__ffmpeg_options, out_filename])
+                self.__debug_print__(shell_command)
                 subprocess.run(shell_command, shell=True)
                 continue
             except:
@@ -90,20 +105,27 @@ class Resampling:
         return
 
 
-    def debug_print(self, string):
-        if self.debug:
-            print('DEBUG: ' + string)
+    def __debug_print__(self, dbg_data):
+        if self.__debug:
+            print('DEBUG: ', end='')
+            print(dbg_data)
         return
 
 
 def main():
     parser = argparse.ArgumentParser(description='Convert multiple audio files using FFmpeg')
     parser.add_argument('-p', '--profile', type=str, default='', help='Specify pre-set config json file')
+    parser.add_argument('-i', '--input_filetype', type=str, default='', help='Specify input filetype')
+    parser.add_argument('-o', '--output_filetype', type=str, default='', help='Specify output filetype (flac, mp3 or aac).')
     args = parser.parse_args()
 
     resampler = Resampling()
     if len(args.profile) > 0:
         resampler.set_profile(args.profile)
+    if len(args.input_filetype) > 0:
+        resampler.set_input_filetype(args.input_filetype)
+    if len(args.output_filetype) > 0:
+        resampler.set_output_filetype(args.output_filetype)
 
     resampler.configure()
     resampler.exec()
